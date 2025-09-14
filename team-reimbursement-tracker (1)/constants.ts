cec43245655c8849ca40046aa02b2dd54b4472b8
11-levin <1119274756@qq.com>
2025-09-14 16:49:56 +0800

    Add files via upload

---
+++ team-reimbursement-tracker (1)/constants.ts
@@ -0,0 +1,13 @@
+import type { Member } from './types';
+
+export const INITIAL_MEMBERS: Member[] = [
+  { id: 1, name: '荣', totalAmount: 0, studentId: '' },
+  { id: 2, name: '何奥雄', totalAmount: 0, studentId: '' },
+  { id: 3, name: '邹彪', totalAmount: 0, studentId: '' },
+  { id: 4, name: '朱柏铮', totalAmount: 0, studentId: '' },
+  { id: 5, name: 'bigsister', totalAmount: 0, studentId: '' },
+  { id: 6, name: '卢天翼', totalAmount: 0, studentId: '' },
+  { id: 7, name: '孙萌', totalAmount: 0, studentId: '' },
+  { id: 8, name: '矛哥', totalAmount: 0, studentId: '' },
+  { id: 9, name: '范巧林', totalAmount: 0, studentId: '' },
+];
